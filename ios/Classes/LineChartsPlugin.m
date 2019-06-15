c4fe7323b79d11d670880880843a754c3458aea6
Damián Rincón <damianrincondrc@gmail.com>
2019-06-15 01:06:55 -0500

    init commit

---
+++ ios/Classes/LineChartsPlugin.m
@@ -0,0 +1,20 @@
+#import "LineChartsPlugin.h"
+
+@implementation LineChartsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"line_charts"
+            binaryMessenger:[registrar messenger]];
+  LineChartsPlugin* instance = [[LineChartsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
---
+++ lib/main.dart
@@ -0,0 +1,3 @@
+library flutter_sparkline;
+
+export 'src/line_chats.dart';
